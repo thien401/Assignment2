75c8bb961944079d97fc8cbbf0ef0c3f085c01cf
LêDuyThiện <thienduylea123@gmail.com>
2025-08-13 10:29:23 +0700

    Add files via upload

---
+++ Exercise1.java
@@ -0,0 +1,17 @@
+
+public class Exercise1 {
+    public static void main(String[] args) {
+        int x1=1+2;
+        System.out.println("1+2="+x1);
+        double x2=3.5+8;
+        System.out.println("3.5+8="+x2);
+        double x3=1.0/2.0;
+        System.out.println("1/2="+x3);
+        double x4=4.5+7;
+        System.out.println("4.5+7="+x4);
+        int x5=4*5+1;
+        System.out.println("4*5+1="+x5);
+        int x6=20%3+2-7;
+        System.out.println("20%3+2-7="+x6);
+    }
+}
---
+++ Exercise21.java
@@ -0,0 +1,9 @@
+
+public class Exercise21 {
+    public static void main(String[] args) {
+        int x=1;
+        int y=x + ++x - x-- - x++ + x - x--;
+        System.out.println("x="+x);
+        System.out.println("y="+y);
+    }
+}
